8fe4c430fc64eb0f24dd439680f2713dd322ba3b
Kuvuklija <cerakuzz@mail.ru>
2018-12-06 23:04:43 +0300

    ver 2

--- ControllersAndActions/Controllers/DerivedController.cs
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -1,17 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ControllersAndActions.Controllers
-{
-    public class DerivedController : Controller
-    {
-        public ActionResult Index()
-        {
+namespace ControllersAndActions.Controllers{
+
+    public class DerivedController : Controller{
+    
+        public ActionResult Index(){
             ViewBag.Message = "Hello from the DerivedController Index method";
             return View("MyView");
+        }
+
+        public ActionResult CustomContextData() {
+             string userName = User.Identity.Name;
+             string serverName = Server.MachineName;
+             string clientIP = Request.UserHostAddress;
+             string clientBrowser = Request.Browser.Browser;
+             DateTime dateStamp = HttpContext.Timestamp;
+
+             ViewBag.clientBrowser = clientBrowser;
+             ViewBag.clientIP = clientIP;
+
+             string oldProductName = Request.Form["OldName"];
+             string newProductName = Request.Form["NewName"];
+             return View((object)clientIP);
         }
     }
 }
