25d0ea897ca7e1b880330c50fbc03e55ea09174d
Kuvuklija <cerakuzz@mail.ru>
2018-12-10 23:15:04 +0300

    ver 4

--- ControllersAndActions/Controllers/BasicController.cs
+++ ControllersAndActions/Controllers/BasicController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,14 +7,17 @@ using System.Web.Routing;
 
 namespace ControllersAndActions.Controllers
 {
-    public class BasicController : IController
-    {
-        public void Execute(RequestContext requestContext) {
+  public class BasicController : IController
+  {
+    public void Execute(RequestContext requestContext) {
 
-            string controller = (string)requestContext.RouteData.Values["controller"];
-            string action = (string)requestContext.RouteData.Values["action"];
-
-            requestContext.HttpContext.Response.Write(string.Format("Controller: {0}, Action: {1}", controller, action));
-        }
+     string controller = (string)requestContext.RouteData.Values["controller"];
+     string action = (string)requestContext.RouteData.Values["action"];
+      if (action.ToLower() == "redirect") {
+        requestContext.HttpContext.Response.Redirect("/Derived/Index");
+      }else {
+        requestContext.HttpContext.Response.Write(string.Format("Controller: {0}, Action: {1}", controller, action));
+      }
     }
+  }
 }
--- ControllersAndActions/Controllers/DerivedController.cs
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ControllersAndActions.Infrastructure;
 
 namespace ControllersAndActions.Controllers{
 
@@ -22,10 +23,24 @@ namespace ControllersAndActions.Controllers{
 
              ViewBag.clientBrowser = clientBrowser;
              ViewBag.clientIP = clientIP;
+             ViewBag.server = Server.MachineName;
 
              string oldProductName = Request.Form["OldName"];
              string newProductName = Request.Form["NewName"];
              return View((object)clientIP);
         }
+
+        //public void ProduceOutput() {
+        public ActionResult ProduceOutput() { 
+            if (Server.MachineName == "LAPTOP-CLK2NFEV") {
+        //Response.Redirect("/Basic/Index");
+        //return new CustomRedirectResult { Url = "/Basic/Index" };
+        //return new RedirectResult("/Basic/Index");
+        return Redirect("/Basic/Index");
+            }else{
+                Response.Write("Controller: "+RouteData.Values["contoller"]+" Action: "+ RouteData.Values["action"]);
+                return null;
+            }
+    }
     }
 }
