5fb3124af9ad986bb5a50e214c22d83053ea0ebe
Kuvuklija <cerakuzz@mail.ru>
2018-12-20 21:25:43 +0300

    v4

--- ControllersAndActions.Tests/UnitTest1.cs
+++ ControllersAndActions.Tests/UnitTest1.cs
@@ -14,9 +14,13 @@ namespace ControllersAndActions.Tests
           ExampleController target = new ExampleController();
           //action
           ViewResult result = target.Index();
-         //assert
-         //Assert.AreEqual("Homepage", result.ViewName);
-          Assert.AreEqual("", result.ViewName);
+      //assert
+      //Assert.AreEqual("Homepage", result.ViewName); //if ViewResult("Homepage")
+      //Assert.AreEqual("", result.ViewName);          //if ViewResult()
+      //Assert.AreEqual("~/Views/Derived/MyView.cshtml", result.ViewName);      //if ViewResult("~/Views/Derived/MyView")
+      Assert.AreEqual("", result.ViewName);
+      Assert.IsInstanceOfType(result.ViewData.Model, typeof(System.DateTime));
+      Assert.AreEqual("Hello",result.ViewBag.Hi);
         }
     }
 }
